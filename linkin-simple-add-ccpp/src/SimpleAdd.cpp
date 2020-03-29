e799e4d2585b0bc8178a574573661904daeb350a
numb-men <le@hengyumo.cn>
2020-03-29 16:48:06 +0800

    update

--- linkin-simple-add-ccpp/src/SimpleAdd.cpp
+++ linkin-simple-add-ccpp/src/SimpleAdd.cpp
@@ -9,7 +9,7 @@ using namespace std;
 vector<string> split(const string& str, const string& delim) {
     vector<string> res;
     if("" == str) return res;
-    //先将要切割的字符串从string类型转换为char*类型
+    //鍏堝皢瑕佸垏鍓茬殑瀛楃涓蹭粠string绫诲瀷杞崲涓篶har*绫诲瀷
     char * strs = new char[str.length() + 1];
     strcpy(strs, str.c_str());
 
@@ -18,10 +18,10 @@ vector<string> split(const string& str, const string& delim) {
 
     char *p = strtok(strs, d);
     while(p) {
-        //分割得到的字符串转换为string类型
+        //鍒嗗壊寰楀埌鐨勫瓧绗︿覆杞崲涓簊tring绫诲瀷
         string s = p;
         res.push_back(s);
-        //存入结果数组
+        //瀛樺叆缁撴灉鏁扮粍
         p = strtok(NULL, d);
     }
 
@@ -30,14 +30,14 @@ vector<string> split(const string& str, const string& delim) {
 
 int main(int argc, char *argv[]) {
 
-	char* input, * output;
-	for (int i = 0; i < argc; i++) {
-		if (strcmp(argv[i], "-input") == 0) {
-			input = argv[++i];
-		} else if (strcmp(argv[i], "-output") == 0) {
-			output = argv[++i];
-		}
-	}
+    char* input, * output;
+        for (int i = 0; i < argc; i++) {
+            if (strcmp(argv[i], "-input") == 0) {
+                input = argv[++i];
+        } else if (strcmp(argv[i], "-output") == 0) {
+            output = argv[++i];
+        }
+    }
 //	cout << input << '\n' << output << endl;
     ifstream fin;
     fin.open(input);
@@ -54,10 +54,11 @@ int main(int argc, char *argv[]) {
     fin.close();
     ofstream fout(output);
     for (vector<int>::iterator it = sums.begin(); it != sums.end(); it++) {
-        cout << *it << '\n';
+        // cout << *it << '\n';
         fout << *it << '\n';
     }
-    cout << endl;
+    // cout << endl;
     fout.close();
-    cin.get();
+    // 涓嶆敞閲婁細瓒呮椂
+    // cin.get();
 }
